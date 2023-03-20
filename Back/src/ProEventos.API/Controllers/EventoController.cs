d86f3580296322fbb0954098f6b3b5c96cc61ad3
pQuintans <caixote.dom@gmail.com>
2023-03-20 14:50:04 -0300

    Seção 2: .Net 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+                new Evento() {
+                    EventoId = 1,
+                    Local = "São Paulo",
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    Tema = "Angular e .NET Core",
+                    QtdPessoas = 250,
+                    Lote = "1º Lote",
+                    ImagemURL = "foto1.png"
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Local = "São Paulo",
+                    DataEvento = DateTime.Now.AddDays(12).ToString("dd/MM/yyyy"),
+                    Tema = "Angular e .NET Core",
+                    QtdPessoas = 250,
+                    Lote = "1º Lote",
+                    ImagemURL = "foto1.png"
+                }
+        };
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]	
+        public IEnumerable<Evento> Get(int id)
+        {
+            return _evento.Where(e => e.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "post";
+        }
+    }
+}
